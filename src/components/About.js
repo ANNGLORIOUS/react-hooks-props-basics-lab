ce1f36d541c18c82bd32a54b25120d0362537bc7
ANNGLORIOUS <mueniannglorious@gmail.com>
2024-08-03 16:46:40 +0300

    initial commit

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,14 @@
-import React from "react";
+// About.js
+import React from 'react';
+import Links from './Links';
 
-function About() {
+const About = (props) => {
   return (
-    <div id="about">
-      <h2>About Me</h2>
-      <p>Put the bio in here</p>
-      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+    <div id='about'>
+      {props.bio && <p>{props.bio}</p>}
+      <Links github={props.github} linkedin={props.linkedin} />
     </div>
   );
-}
+};
 
 export default About;
